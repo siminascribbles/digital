c65b12183fb107ae0fd1ea7bc5101bc28975fec0
siminascribbles <simina.popescu356@gmail.com>
2017-03-25 08:55:32 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+createCanvas (430, 800)}
+
+function draw() {
+ background(237, 239, 242)
+ strokeWeight(3)
+ line (49,776,165,65)
+ line(165,65,312,748)
+ line(53,185,295,162)
+ rect(3,mouseY,318,7)
+ line(94,283,254,283)
+ line(94,305,254,305)
+ line(94,329,254,329)
+ rect(mouseX,352,177,11)
+ rect (89,376,170,8)
+ fill(0,0,0)
+ rect(mouseX,488,279,10)
+ line(1,782,343,745)
+ line(1,787,341,749)
+  
+}
